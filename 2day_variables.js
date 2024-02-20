efdb8e4c544a50601534d465f07e9c01c1bf1e7f
manoj kumar <112846756+manojwebdevelopment@users.noreply.github.com>
2024-02-20 17:17:29 +0530

    2day commit

---
+++ 2day_variables.js
@@ -0,0 +1,12 @@
+ const id = 124563
+ let Email = "manoj@gmail.com"
+ var password = "123456"
+
+ //id = 78945 can't allowed
+
+ Email = "manoj121@gmail.com"
+ password = "12345999"
+
+ console.table([
+    Email , password
+ ])
